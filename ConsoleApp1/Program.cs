e514425c05d1987b5fc7a839e4f8cadf75b75106
LikeJust <diddydwns135@gmail.com>
2018-03-29 19:52:21 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace ConsoleApp1
+{
+    class Program 
+    {
+        private static void Main(string[] args)
+        {
+            var sourceObservable = Observable.Range(1, 5);
+            
+            var subscriber = sourceObservable.Subscribe(
+                x => Console.WriteLine("OnNext: {0}", x),
+                ex => Console.WriteLine("OnError: {0}", ex.Message),
+                () => Console.WriteLine("OnCompleted"));
+
+            subscriber.Dispose();
+
+            var studentFactory = new StudentFactory();
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        private ArrayList _scholarshipArray = new ArrayList();
+
+        public Student()
+        {
+
+        }
+
+        public int StudentNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string Major { get; set; }
+    }
+}
